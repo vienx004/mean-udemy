70ed310db4f25e0a7457cb83fbd740fe5981f954
vienx004 <derek.h.vien@gmail.com>
2017-01-03 14:10:17 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
 var appRoutes = require('./routes/app');
+var messageRoutes = require('./routes/message');
 
 var app = express();
 mongoose.connect('localhost:27017/node-angular');
@@ -30,7 +31,9 @@ app.use(function (req, res, next) {
     next();
 });
 
+app.use('/message', messageRoutes);
 app.use('/', appRoutes);
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
